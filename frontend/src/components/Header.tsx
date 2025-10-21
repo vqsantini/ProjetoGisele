97cc115870f0e0f68b26ce7a12e180d10ae86809
vqsantini <victor.g.santz@gmail.com>
2025-10-21 16:40:27 -0300

    fazendo teste de conexão

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,7 +6,7 @@ export function Header() {
             <div className="basis-3/5 flex justify-start pl-20 md:flex-1">
                 <p className="text-white font-rubik-glitch text-3xl cursor-default ">#HomofobiaÉCrime</p>
             </div>
-            <div className="text-white flex gap-14 font-bebas-neue text-2xl basis-2/5 justify-center items-center gap-14 text-nowrap hidden lg:flex">
+            <div className="text-white flex gap-14 font-bebas-neue text-4xl basis-2/5 justify-center items-center gap-14 text-nowrap hidden lg:flex">
                 <Link to='/'>INÍCIO</Link>
                 <Link to='/pesquisa'>PESQUISA</Link>
                 <Link to='https://www.gov.br/pt-br/servicos/denunciar-violacao-de-direitos-humanos'><button className="py-2 px-5 bg-red-500 rounded hover:bg-red-600 ">DENUNCIA? DISQUE 100!</button></Link>
